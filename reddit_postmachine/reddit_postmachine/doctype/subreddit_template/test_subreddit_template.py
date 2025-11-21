5d7064375f9a70fe919f15fa3d3a8f1507a0e517
AndrewMaraiev <105721308+AndrewMaraiev@users.noreply.github.com>
2025-11-21 14:01:17 +0000

    feat: initial commit - Subreddit Template DocType

---
+++ reddit_postmachine/reddit_postmachine/doctype/subreddit_template/test_subreddit_template.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, AndrewM and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSubredditTemplate(FrappeTestCase):
+	pass
